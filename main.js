1272eb496dccbed746be6cdb3ec13803b7b7aa13
Sambat Lim <sambatlim86@gmail.com>
2018-09-06 21:24:45 +0700

    add website

---
+++ main.js
@@ -0,0 +1,17 @@
+function getGrade() {
+  var score, result, fromInput;
+  const n = 88488;
+  fromInput = document.getElementById("InputScore").value;
+  score = parseFloat(fromInput);
+  if (score == "NaN") {
+    result = "សូមវាយជាលេខក្នុងប្រអប់";
+  } else if (score <= 0 || score > 100) {
+    result = "សូមបញ្ចូលលេខអោយបានត្រឹមត្រូវ";
+  } else {
+    var x = 100 - score;
+    var y = x * n;
+    var z = y + 100;
+    result = Math.floor(z / 100);
+  }
+  document.getElementById("showGrade").innerHTML = result;
+}
